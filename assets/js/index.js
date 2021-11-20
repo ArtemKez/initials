3ba22f4a89bdbfb178c7a2c10900f23397170c2e
ArtemKez <artem.kez.work@gmail.com>
2021-11-20 17:28:05 +0200

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,59 +2,67 @@
 
 const cardsContainer = document.getElementById('cardsContainer');
 
-const HTMLElements = actors.map((actor)=>createActorCards(actor));
+const HTMLElements = actors.map((actor) => createActorCards(actor));
 
-function createActorCards(actor){
-  const card = document.createElement('li');
-  card.classList.add('cardWrapper');
+function createActorCards(actor) {
+    const card = document.createElement('li');
+    card.classList.add('cardWrapper');
 
-  const container = document.createElement('article');
-  container.classList.add('cardContainer');
+    const container = document.createElement('article');
+    container.classList.add('cardContainer');
 
-  const imgWrapper = document.createElement('div');
-  imgWrapper.classList.add('cardImageWrapper');
+    const imgWrapper = document.createElement('div');
+    imgWrapper.classList.add('cardImageWrapper');
 
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(actor.name[0] || 'noname'));
-  initials.style.backgroundColor = stringToColour(actor.name || '');
+    const initials = document.createElement('div');
+    initials.classList.add('initials');
 
-  const img = document.createElement('img');
-  img.classList.add('cardImage');
-  img.setAttribute('src', actor.photo);
-  img.setAttribute('alt', actor.name);
-  img.addEventListener('error', handleImageError);
+    if (typeof actor.name === 'string') {
+        let actorName = actor.name.split(' ')
+        initials.append(document.createTextNode(`${actorName[0][0]} ${actorName[1][0]}`));
+    } else {
+        initials.append(document.createTextNode('noname'));
+    }
+    initials.style.backgroundColor = stringToColour(actor.name || '');
 
-  imgWrapper.append(initials, img);
+    const img = document.createElement('img');
+    img.classList.add('cardImage');
+    img.setAttribute('src', actor.photo);
+    img.setAttribute('alt', actor.name);
+    img.addEventListener('error', handleImageError);
 
-  const name = document.createElement('h2');
-  name.classList.add('cardName');
-  name.append(document.createTextNode(actor.name || 'noname'));
+    imgWrapper.append(initials, img);
 
-  const description = document.createElement('p');
-  description.classList.add('cardDescription');
-  description.append(document.createTextNode(actor.birthdate || 'unknow'));
+    const name = document.createElement('h2');
+    name.classList.add('cardName');
+    name.append(document.createTextNode(actor.name || 'noname'));
 
-  container.append(imgWrapper, name, description);
-  card.append(container);
-  return card;
+    const description = document.createElement('p');
+    description.classList.add('cardDescription');
+    description.append(document.createTextNode(actor.birthdate || 'unknow'));
+
+    container.append(imgWrapper, name, description);
+    card.append(container);
+    return card;
 }
+
 cardsContainer.append(...HTMLElements);
 
 /*  handles */
-function handleImageError({target}){
-  target.remove();
+function handleImageError({target}) {
+    target.remove();
 }
+
 /* utilits */
 function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    let value = (hash >> (i * 8)) & 0xFF;
-    colour += ('00' + value.toString(16)).substr(-2);
-  }
-  return colour;
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
 }
